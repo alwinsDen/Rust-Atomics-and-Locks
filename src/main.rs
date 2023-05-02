6310c506cafa38beb0c88a2f1d494cf84948c3e4
alwinsDen <jsmadesimple@gmail.com>
2023-05-02 15:30:47 +0530

    exp1: The main thread here shuts down before rest of the threads are completed. Hence they are not fully printed.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::{
+    cell::{Cell, RefCell, UnsafeCell},
+    collections::VecDeque,
+    marker::PhantomData,
+    mem::{ManuallyDrop, MaybeUninit},
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+    rc::Rc,
+    sync::{*, atomic::{*, Ordering::*}},
+    thread::{self, Thread},
+};
+
+fn main() {
+    thread::spawn(f);
+    thread::spawn(f);
+    println!("Hello from the main thread.");
+}
+
+fn f() {
+    println!("Hello from another thread!");
+    let id = thread::current().id();
+    println!("This here is my thread {id:?}");
+}
