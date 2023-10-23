9a518395ad60f5663891e64d9b7afc5497eefbbb
alwinvarghese <jsmadesimple@gmail.com>
2023-10-23 13:40:14 +0530

    Leaking with Box and reference counting!.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 //Low level Concurrency in Rust
 //Notion Sheet: https://scratched-salute-050.notion.site/OS-and-Compilers-in-Rust-8791003a4cd4454183ff3dadb8456b63?pvs=4
-
 use std::thread;
+use std::boxed::Box;
+
 fn main(){
     // CHAPTER 1
-    //scoped Threads;
+    // Reference counting.
+    self::snip8();
+    // Leaking with Box
+    self::snip7();
+    // Statics from shared ownership and reference counting.
+    self::snip6();
+    // scoped Threads;
     self::snip5();
-    //call a closure with Thread Builder method.
+    // call a closure with Thread Builder method.
     self::snip4();
-    //getting value back from a thread.
+    // getting value back from a thread.
     self::snip3();
-    //using closures instead of Funcs()
+    // using closures instead of Funcs()
     self::snip2();
-    //basics of concurrency in Rust.
+    // basics of concurrency in Rust.
     self::snip1();
+}
+fn snip8(){
+
+}
+fn snip7(){
+    //Box::leak leaks the contents of the Box.
+    //this is a static type, meaning it can be
+    //borrowed across the program.
+    let x : &'static [i32; 3] = Box::leak(Box::new([1,2,3]));
+    let f = thread::spawn(move|| dbg!(x));
+    let f= thread::spawn(move ||dbg!(x));
+    f.join().unwrap();
+}
+fn snip6(){
+    static X : [i32; 3] = [1,2,3];//static vars are never dropped.
+    thread::spawn(|| dbg!(&X));
+    thread::spawn(|| dbg!(&X));
 }
 fn snip5(){
     let numbers = vec![1,2,3,5,6,3];
